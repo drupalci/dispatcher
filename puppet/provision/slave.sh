2e81caf745dc314fabab7b710fbedd278f9059fa
Nick Schuch <nick@myschuch.com>
2014-08-10 21:26:45 +1000

    Rework so slaves work.

--- puppet/provision/slave.sh
+++ puppet/provision/slave.sh
@@ -7,6 +7,8 @@ DIR='/vagrant/puppet'
 
 export FACTER_vagrant="1"
 export FACTER_jenkins_role="slave"
-export FACTER_jenkins_master="master.local"
+export FACTER_jenkins_master="192.168.1.52"
+export FACTER_jenkins_master_user="admin"
+export FACTER_jenkins_master_pass="password"
 
 cd $DIR && sudo -E puppet apply --modulepath $DIR/modules --hiera_config=$DIR/etc/hiera.yaml $DIR/site.pp
