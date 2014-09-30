626238c4fe9ff0c83f797a97eabf5b5ae2fae8a8
Nick Schuch <nick@myschuch.com>
2014-09-30 21:43:38 +1000

    Update script run for AMI build.

--- packer/ubuntu/scripts/ami-spot.sh
+++ packer/ubuntu/scripts/ami-spot.sh
@@ -5,13 +5,15 @@
 # Description: Install extra configuration for AWS Spot instances.
 
 echo "Updating package list"
-sudo apt-get update
+apt-get update
 echo "Installing dependencies"
-sudo apt-get install wget python -y
+apt-get install wget python -y
 
 echo "Downloading boot script"
-sudo wget https://raw.githubusercontent.com/jenkinsci/ec2-plugin/master/src/main/webapp/AMI-Scripts/ubuntu-init.py -O /usr/bin/userdata
-sudo chmod +x /usr/bin/userdata
+wget https://raw.githubusercontent.com/jenkinsci/ec2-plugin/master/src/main/webapp/AMI-Scripts/ubuntu-init.py -O /usr/bin/userdata
+chmod +x /usr/bin/userdata
 
 echo "Adding boot script to run after boot is complete"
-sudo sed -i 's/exit 0/python \/usr\/bin\/userdata\n&/' /etc/rc.local
+sed -i 's/exit 0//' /etc/rc.local
+echo "python /usr/bin/userdata &" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
