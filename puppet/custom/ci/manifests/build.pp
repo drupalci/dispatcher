a6e34e95820ca966ec4f4292ecd499c7b2ddfb99
Nick Schuch <nick@myschuch.com>
2014-10-25 13:31:33 +1000

    Update build vars.

--- puppet/custom/ci/manifests/build.pp
+++ puppet/custom/ci/manifests/build.pp
@@ -2,35 +2,35 @@
 #
 # Parameters.
 #
-#   display_name    - A human readable name.
-#   description     - A human readable description about the build.
-#   parameters      - Parameters that can be passed to the build.
-#   build_template  - The build configruation in XML format.
-#   script_template - Ability to override the build script on a per build basis.
-#   jobs_dir        - Jenkins jobs directory path. Shouldn't need to override this.
+#   display_name - A human readable name.
+#   description  - A human readable description about the build.
+#   parameters   - Parameters that can be passed to the build.
+#   config       - The build configruation in XML format.
+#   script       - Ability to override the build script on a per build basis.
+#   jobs_dir     - Jenkins jobs directory path. Shouldn't need to override this.
 #
 # Example.
 #
 #   ci::build { 'example':
-#     display_name    => 'Example',
-#     description     => 'This is an example',
-#     script_template => 'mymodule/script.sh.erb',
+#     display_name => 'Example',
+#     description  => 'This is an example',
+#     script       => 'mymodule/script.sh.erb',
 #   }
 #
 define ci::build (
   # Metadata.
-  $display_name    = undef,
-  $description     = undef,
+  $display_name = undef,
+  $description  = undef,
 
   # Parameters.
-  $parameters      = [],
+  $parameters   = [],
 
   # Templates.
-  $build_template  = 'ci/jobs/build.xml.erb',
-  $script_template = 'ci/jobs/build.sh.erb',
+  $config       = 'ci/jobs/build.xml.erb',
+  $script       = 'ci/jobs/build.sh.erb',
 
   # Directories.
-  $jobs_dir        = '/var/lib/jenkins/jobs',
+  $jobs_dir     = '/var/lib/jenkins/jobs',
 ){
 
   ##
@@ -45,13 +45,13 @@ define ci::build (
   ##
 
   # Jenkins config.
-  $script = template($script_template)
+  $script_output = template($script)
   file { "${jobs_dir}/${title}":
     ensure  => 'directory',
     require => Package['jenkins'],
   }
   file { "${jobs_dir}/${title}/config.xml":
-    content => template($build_template),
+    content => template($config),
     require => File["${jobs_dir}/${title}"],
   }
 
