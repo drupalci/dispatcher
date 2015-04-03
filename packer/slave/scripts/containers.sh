0bd654fd85a2ece5bd65a7636c0aafccac3ef4f7
Nick Schuch <nick@myschuch.com>
2015-04-02 18:45:57 -0700

    Pull containers instead of build.

--- packer/slave/scripts/containers.sh
+++ packer/slave/scripts/containers.sh
@@ -4,4 +4,29 @@
 # Author:      Nick Schuch (nick@myschuch.com)
 # Description: Pull down the containers required for a build via the make command.
 
-# Pull containers here...
+# Used later when a build runs.
+git clone https://github.com/drupalci/drupalci_testbot.git /opt/testbot
+
+IMAGES=(
+"base"
+"web-base"
+"db-base"
+"php-base"
+"web-5.4"
+"web-5.6"
+"web-5.5"
+"web-7"
+"web-5.3"
+"db-mysql-5.5"
+"mysql-5.5"
+"mongodb-2.6"
+"pgsql-9.1"
+"pgsql-9.4"
+"mariadb-5.5"
+"mariadb-10.0"
+)
+
+for i in "${IMAGES[@]}"
+do
+	docker pull drupalci/${i}
+done
